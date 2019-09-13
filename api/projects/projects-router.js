f121340dea661770b9d018662f17db81b3b425cb
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-13 12:13:01 -0600

    Changed 0s and 1s to true false with .map in endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,39 +1,48 @@
-const express = require('express');
+const express = require("express");
 
-const Projects = require('./projects-model');
+const Projects = require("./projects-model");
 
 const router = express.Router();
 
-function validateProject(req, res, next){
-    const project = req.body;
-    if(!project.name){
-        res.status(400).json({message: 'Missing required name field'}).end()
-    }
-    // else if(!project.project_completed){
-    //     res.status(400).json({message: 'Missing required completed field'}).end()
-    // }
-    // else if(typeof project.project_completed != boolean){
-    //     res.status(400).json({message: 'Completed must be of type Boolean'}).end()
-    // }
-    next();
+function validateProject(req, res, next) {
+  const project = req.body;
+  if (!project.name) {
+    res
+      .status(400)
+      .json({ message: "Missing required name field" })
+      .end();
+  }
+  // else if(!project.project_completed){
+  //     res.status(400).json({message: 'Missing required completed field'}).end()
+  // }
+  // else if(typeof project.project_completed != boolean){
+  //     res.status(400).json({message: 'Completed must be of type Boolean'}).end()
+  // }
+  next();
 }
 
-router.post('/', (req, res) => {
-    Projects.addProject(req.body)
-    .then(response => {
-        res.status(200).json(response)
-    })
-})
+router.post("/", (req, res) => {
+  Projects.addProject(req.body).then(response => {
+    res.status(200).json(response);
+  });
+});
 
-router.get('/', (req, res) => {
-    Projects.getProjects()
+router.get("/", (req, res) => {
+  Projects.getProjects()
     .then(response => {
-        res.status(200).json(response)
+      const boolResponse = response.map(obj => {
+        return {
+          id: obj.id,
+          project_name: obj.project_name,
+          project_description: obj.project_description,
+          project_completed: Boolean(obj.project_completed)
+        };
+      });
+      res.status(200).json(boolResponse);
     })
     .catch(error => {
-        res.status(500).json(error)
-    })
-})
-
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
--- api/tasks/tasks-router.js
+++ api/tasks/tasks-router.js
@@ -1,25 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-const Tasks = require('./tasks-model');
+const Tasks = require("./tasks-model");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    Tasks.addTask(req.body)
-    .then(response => {
-        res.status(200).json(response)
-    })
-})
+router.post("/", (req, res) => {
+  Tasks.addTask(req.body).then(response => {
+    res.status(200).json(response);
+  });
+});
 
-router.get('/', (req, res) => {
-    Tasks.getTasks()
+router.get("/", (req, res) => {
+  Tasks.getTasks()
     .then(response => {
-        res.status(200).json(response)
+      const boolResponse = response.map(obj => {
+        return {
+          id: obj.id,
+          project_name: obj.project_name,
+          project_description: obj.project_description,
+          project_completed: Boolean(obj.project_completed),
+          project_id: obj.project_id,
+          task_description: obj.task_description,
+          task_notes: obj.task_notes,
+          task_completed: Boolean(obj.task_completed)
+        };
+      });
+      res.status(200).json(boolResponse);
     })
     .catch(error => {
-        res.status(500).json(error)
-    })
-})
-
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;

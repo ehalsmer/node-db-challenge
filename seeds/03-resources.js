492d63dcd025875fd8ba3f92af090ab58740c8a8
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-13 10:33:30 -0600

    data seeded

--- seeds/03-resources.js
+++ seeds/03-resources.js
@@ -1,13 +1,9 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('resources').insert([
+    {id: 1, name: 'Computer', description: 'Useful for coding'},
+    {id: 2, name: 'Training Kit', description: 'Great for reference and review'},
+    {id: 3, name: 'Instructors', description: 'Available to help'},
+    {id: 4, name: 'Google', description: 'Always there for you'},
+  ]);
 };
